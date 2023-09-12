16df607b60e12e85f22858119ac9b06084d85121
dummy2004 <p.klee.4bdb@gmail.com>
2023-09-12 02:49:32 -0600

    update ejercicio 2

--- HojaDeTrabajo01/Ejercicio02.cpp
+++ HojaDeTrabajo01/Ejercicio02.cpp
@@ -1,6 +1,19 @@
 #include "Ejercicio02.h"
+#include  <algorithm>
 
 bool Ejercicio02::isPalindrome(string& s)
 {
-    return false;
+    int a = 0;
+    int b = s.length() -  1;
+
+    for (int i = 0; i < s.length(); i++) {
+        if ((s[a]) == (s[b])) {
+            a++;
+            b--;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
 }
